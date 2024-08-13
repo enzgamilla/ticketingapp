8571cfd4397db9cd7ff951a77a1be4f2e92cf194
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-13 15:15:43 +0800

    handleform submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,17 +3,41 @@
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import Link from "next/link";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface TicketForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TicketForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title" radius="large" />
-      <SimpleMDE placeholder="Descriptions...." />
-      <Button>
-        <Link href="/issues">Submit new Ticket</Link>
-      </Button>
-    </div>
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        radius="large"
+        {...register("title")}
+      />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Descriptions...." {...field} />
+        )}
+      />
+
+      <Button>Submit new Ticket</Button>
+    </form>
   );
 };
 
