7ed72ef5b2a943790e32244a0a12d34630775aaf
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-13 14:43:39 +0800

    build markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,15 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+"use client";
+
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import Link from "next/link";
-import React from "react";
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" radius="large" />
-      <TextArea variant="surface" placeholder="Descriptions...." />
+      <SimpleMDE placeholder="Descriptions...." />
       <Button>
         <Link href="/issues">Submit new Ticket</Link>
       </Button>
