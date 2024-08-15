994c66c9a4cf5296cf90e46476ed028cb6b93b86
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-15 13:56:49 +0800

    add edit ticket button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { StatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -18,25 +20,33 @@ const TicketDetailedPage = async ({ params }: Props) => {
   if (!ticket) notFound();
 
   return (
-    <>
-      <div className="prose space-y-2">
-        <Heading>{ticket?.title}</Heading>
-        <div className="flex justify-between">
-          <StatusBadge status={ticket.status} />
-          <Text>
-            {ticket?.createdAt.toLocaleDateString("en-us", {
-              weekday: "long",
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-            })}
-          </Text>
+    <Grid columns={{ initial: "1", md: "2" }} gap="7">
+      <Box>
+        <div className="prose space-y-2">
+          <Heading>{ticket?.title}</Heading>
+          <div className="flex justify-between">
+            <StatusBadge status={ticket.status} />
+            <Text>
+              {ticket?.createdAt.toLocaleDateString("en-us", {
+                weekday: "long",
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}
+            </Text>
+          </div>
         </div>
-      </div>
-      <Card className="prose" mt="5">
-        <ReactMarkdown>{ticket?.description}</ReactMarkdown>
-      </Card>
-    </>
+        <Card className="prose" mt="5">
+          <ReactMarkdown>{ticket?.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issue/${ticket.id}/edit`}>Edit Ticket</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
