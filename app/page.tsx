308b57682df2797dac84ddc2421c9a43d05d4af8
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-16 18:50:03 +0800

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div className="bg-white">
       <h1 className="font-semibold text-2xl">Home Page</h1>
+      <Pagination
+        itemCount={22}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </div>
   );
 }
