3298ca6d61bf0a092d4ff554523a64ec1a557c80
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-22 15:37:03 +0800

    add relation to user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,16 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcrypt";
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({
     orderBy: { name: "asc" },
     where: { name: { not: "" } },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+    },
   });
 
   return NextResponse.json(users);
